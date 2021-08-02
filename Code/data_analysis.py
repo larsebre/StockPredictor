d5d9f1d8e9092a93cea25fd936d49689a32a84d2
larsebre <larsebre@stud.ntnu.no>
2021-08-02 23:37:27 +0200

    Plotting differences

--- Code/data_analysis.py
+++ Code/data_analysis.py
@@ -2,11 +2,11 @@ import get_stock_data as gsd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    stock_data = gsd.StockData('EQNR.OL', data_days=700, forecast=20)
+    stock_data = gsd.StockData('EQNR.OL', data_days=1400, forecast=20)
     stock_data.initialize()
 
     #Scatter plot to see correlation
-    plt.scatter(stock_data.df_data['CCI20'], stock_data.df_data['Price Change'])
+    plt.scatter(stock_data.df_x_data['S&P 500 Close'], stock_data.df_y_data['Price Change'])
     plt.show()
 
     print(stock_data.df_data)
