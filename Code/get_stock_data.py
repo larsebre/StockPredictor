585d42f3ad13a54149363b7853dea85dbf8fc5b8
larsebre <larsebre@stud.ntnu.no>
2021-08-02 23:37:50 +0200

    Add market index data

--- Code/get_stock_data.py
+++ Code/get_stock_data.py
@@ -1,7 +1,8 @@
+import datetime
+import numpy as np
 from pandas_datareader import data
 import pandas as pd
 import ta
-import datetime
 
 
 class StockData:
@@ -10,38 +11,69 @@ class StockData:
         self.days_forecast = forecast
         self.end_date = datetime.datetime.now()
         self.start_date = self.end_date - datetime.timedelta(days=data_days)
-        self.df_data = pd.DataFrame()
-        self.prediction_input = pd.DataFrame()
+        self.df_x_data = pd.DataFrame()
+        self.df_y_data = pd.DataFrame()
+        self.df_dow_jones = data.DataReader('^DJI', 'yahoo', self.start_date, self.end_date)
+        self.df_SP500 = data.DataReader('^GSPC', 'yahoo', self.start_date, self.end_date)
+        self.num_prediction_inputs = 3
+        self.prediction_inputs = pd.DataFrame()
 
     def set_stock_data(self):
-        self.df_data = data.DataReader(self.TICKER, 'yahoo', self.start_date, self.end_date)
+        self.df_x_data = data.DataReader(self.TICKER, 'yahoo', self.start_date, self.end_date)
 
     def calculate_technical_indicators(self):
-        self.df_data = ta.utils.dropna(self.df_data)
-        self.df_data['MFI'] = ta.volume.MFIIndicator(high=self.df_data['High'], low=self.df_data['Low'], close=self.df_data['Close'], volume=self.df_data['Volume']).money_flow_index()
-        self.df_data['ADI'] = ta.volume.AccDistIndexIndicator(high=self.df_data['High'], low=self.df_data['Low'], close=self.df_data['Close'], volume=self.df_data['Volume']).acc_dist_index() 
-        self.df_data['SMA20'] = ta.trend.SMAIndicator(close=self.df_data['Close'], window=20).sma_indicator()
-        self.df_data['CCI20'] = ta.trend.CCIIndicator(high=self.df_data['High'], low=self.df_data['Low'], close=self.df_data['Close'], window=20, constant=0.015).cci()
-        self.df_data['RSI'] = ta.momentum.RSIIndicator(close=self.df_data['Close']).rsi()
-        self.df_data = self.df_data[['Close', 'ADI', 'MFI', 'SMA20', 'CCI20', 'RSI']]
+        self.df_x_data = ta.utils.dropna(self.df_x_data)
+        self.df_x_data['MFI'] = ta.volume.MFIIndicator(high=self.df_x_data['High'], low=self.df_x_data['Low'], close=self.df_x_data['Close'], volume=self.df_x_data['Volume']).money_flow_index()
+        self.df_x_data['ADI'] = ta.volume.AccDistIndexIndicator(high=self.df_x_data['High'], low=self.df_x_data['Low'], close=self.df_x_data['Close'], volume=self.df_x_data['Volume']).acc_dist_index() 
+        self.df_x_data['SMA20'] = ta.trend.SMAIndicator(close=self.df_x_data['Close'], window=20).sma_indicator()
+        self.df_x_data['CCI20'] = ta.trend.CCIIndicator(high=self.df_x_data['High'], low=self.df_x_data['Low'], close=self.df_x_data['Close'], window=20, constant=0.015).cci()
+        self.df_x_data['MACD'] = ta.trend.MACD(close=self.df_x_data['Close']).macd()
+        self.df_x_data['RSI'] = ta.momentum.RSIIndicator(close=self.df_x_data['Close']).rsi()
+        self.df_x_data['DOW JONES Close'] = self.df_dow_jones['Close']
+        self.df_x_data['DOW JONES SMA20'] = ta.trend.SMAIndicator(close=self.df_dow_jones['Close'], window=20).sma_indicator()
+        self.df_x_data['S&P 500 Close'] = self.df_SP500['Close']
+        self.df_x_data['DOW JONES SMA20'] = ta.trend.SMAIndicator(close=self.df_SP500['Close'], window=20).sma_indicator()
+        self.df_x_data = self.df_x_data[['Close', 'ADI', 'MFI', 'SMA20', 'CCI20', 'MACD', 'RSI', 'DOW JONES Close', 'DOW JONES SMA20', 'S&P 500 Close', 'DOW JONES SMA20']]
 
     #Used to predict 
-    def set_last_day_data(self):
-        self.prediction_input = self.df_data.tail(1)
+    def set_prediction_inputs(self):
+        self.prediction_inputs = self.df_x_data.tail(self.num_prediction_inputs)
 
     #Adding the Price Change n traiding days ahead
     def calculate_price_change(self):
-        self.df_data['Price Change'] = -self.df_data['Close'].diff(periods=-self.days_forecast)
-        self.df_data['Price Change'] = self.df_data['Price Change']/self.df_data['Close']
-        self.df_data = self.df_data.dropna()
+        self.df_x_data['Price Change'] = -self.df_x_data['Close'].diff(periods=-self.days_forecast)
+        self.df_x_data['Price Change'] = self.df_x_data['Price Change']/self.df_x_data['Close']
+        self.df_x_data = self.df_x_data.dropna()
+    
+    def split_data_to_x_and_y(self):
+        self.df_y_data = self.df_x_data[['Price Change']]
+        self.df_x_data.drop(columns='Price Change', inplace=True)
+    
+    #Returns a numpy array to be used for training
+    def get_X_data(self):
+        self.df_x_data = self.df_x_data.append(self.prediction_inputs)
+        return self.df_x_data.to_numpy()
+
+    #Returns a numpy array of 0,1,2 depending on the 'Price Change': 
+    #0 -> Price Change < 0.05, 1 -> 0.05 <= Price Change
+    def get_Y_data(self):
+        classes = []
+        for change in self.df_y_data[['Price Change']].to_numpy():
+            if (change[0] < 0.1):
+                classes.append(0)
+            elif (change[0] >= 0.1):
+                classes.append(1)
+        return np.array(classes)
     
     def initialize(self):
         self.set_stock_data()
         self.calculate_technical_indicators()
-        self.set_last_day_data()
+        self.set_prediction_inputs()
         self.calculate_price_change()
+        self.split_data_to_x_and_y()
 
 if __name__ == "__main__":
-    stock_data = StockData('NEL.OL', data_days=365, forecast=20)
+    stock_data = StockData('KOA.OL', data_days=365, forecast=20)
     stock_data.initialize()
-    print(stock_data.df_data)
+    stock_data.get_X_data()
+    print(stock_data.get_Y_data())
