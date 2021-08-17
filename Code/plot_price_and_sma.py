e62dc266aa0fb588a51d80d60353312266a34277
larsebre <larsebre@stud.ntnu.no>
2021-08-17 20:47:03 +0200

    Add plot_monitored_stocks=True so that all monitored stocks get plotted right away

--- Code/plot_price_and_sma.py
+++ Code/plot_price_and_sma.py
@@ -7,25 +7,34 @@ import plotly.graph_objects as go
 
 if __name__ == "__main__":
 
+    df_monitor = pd.read_excel('monitor_these_stocks.xlsx')
+
+    # Change this if you want to plot single stock
+    plot_monitored_stocks = False 
     # Change ticker and sma to get the right plot
-    ticker = 'NEL.OL'
-    sma = 10
+    ticker = ['VOLUE.OL']
+    sma = [10]
 
-    end_date = datetime.datetime.now()
-    start_date = end_date - datetime.timedelta(days=20*sma)
-    df_stock = data.DataReader(ticker, 'yahoo', start_date, end_date)
-    df_stock['SMA'] = ta.trend.SMAIndicator(close=df_stock['Adj Close'], window=sma).sma_indicator()
-    df_stock = df_stock.dropna()
+    if (plot_monitored_stocks):
+        ticker = df_monitor['TICKER'].to_list()
+        sma = df_monitor['SMA Length'].to_list()
 
-    fig = go.Figure(data=[go.Candlestick(x=df_stock.index,
-                open=df_stock['Open'], high=df_stock['High'],
-                low=df_stock['Low'], close=df_stock['Adj Close'])
-                     ])
-    fig.update_layout(xaxis_rangeslider_visible=False)
-    fig.add_trace(go.Scatter(x=df_stock.index, y=df_stock['SMA'], name='SMA',
-                         line=dict(color='blue', width=4)))
+    for i in range(len(ticker)):
+        end_date = datetime.datetime.now()
+        start_date = end_date - datetime.timedelta(days=20*sma[i])
+        df_stock = data.DataReader(ticker[i], 'yahoo', start_date, end_date)
+        df_stock['SMA'] = ta.trend.SMAIndicator(close=df_stock['Adj Close'], window=sma[i]).sma_indicator()
+        df_stock = df_stock.dropna()
 
-    fig.update_layout(title= ticker + ' SMA' + str(sma),
-                   xaxis_title='Days',
-                   yaxis_title='Price')
-    fig.show()
+        fig = go.Figure(data=[go.Candlestick(x=df_stock.index,
+                    open=df_stock['Open'], high=df_stock['High'],
+                    low=df_stock['Low'], close=df_stock['Adj Close'])])
+
+        fig.update_layout(xaxis_rangeslider_visible=False)
+        fig.add_trace(go.Scatter(x=df_stock.index, y=df_stock['SMA'], name='SMA',
+                            line=dict(color='blue', width=4)))
+
+        fig.update_layout(title= ticker[i] + ' SMA' + str(sma[i]),
+                    xaxis_title='Days',
+                    yaxis_title='Price')
+        fig.show()
